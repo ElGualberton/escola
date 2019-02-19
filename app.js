2bd9c0a8d340874f84657bd48a2f69514b33c155
Gualberto <marcosgualberto@gmail.com>
2019-02-19 13:56:18 -0300

    19 de fevereiro

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ app.listen(3000, function(){
     console.log('subiu');
 });
 
-process.env.APP_CRUD = 'teste';
+// process.env.APP_CRUD = 'c';
 
 console.log('Valor Atualx ' + process.env.APP_CRUD);
