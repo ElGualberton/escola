4ddebd2e19bb03423b078e5c22378369a41c9ce4
Gualberto <marcosgualberto@gmail.com>
2019-02-20 16:25:30 -0300

    .

--- notas/app/routes/admin.js
+++ notas/app/routes/admin.js
@@ -2,7 +2,4 @@ module.exports = function(application){
     application.get('/formulario_inclusao_turma', function(req, res){
         application.notas.app.controllers.admin.formulario_inclusao_turma(application, req, res);
     });
-    application.post('/turmas/salvar', function(req, res){
-        application.notas.app.controllers.admin.turmas_salvar(application, req, res);
-    });
 }
--- notas/app/routes/turmas.js
+++ notas/app/routes/turmas.js
@@ -7,4 +7,7 @@ module.exports = function(application){
         console.log('rotas');
         application.notas.app.controllers.turmas.turma(application, req, res)
     });
+    application.post('/turma/salvar', function(req, res){
+        application.notas.app.controllers.admin.turmas_salvar(application, req, res);
+    });
 }; 
